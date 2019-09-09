fbb97d9943fbf6d21de2c3dd5634dfe58161fdc8
UdonDa <udoooon0727@gmail.com>
2019-09-10 02:41:03 +0900

    add contents

--- pages/index.vue
+++ pages/index.vue
@@ -74,35 +74,237 @@
 
         <v-card
           class="card">
+          <h2>Projects</h2>
+        </v-card>
+
+        <v-layout justify-center class="content--publications" wrap>
+          <v-card class="content--work">
+            <v-card-title class="content--work--title">未踏IT人材発掘・育成事業：2019年度採択プロジェクト</v-card-title>
+            <v-card-text>
+              <p>VR空間における食体験の構築</p>
+              <p><a href="https://twitter.com/signs0302" target="_blank" rel="noopener noreferrer">Kizashi Nakano</a>, <u><b>Daichi Horita</b></u>, Project manager: <a href="https://twitter.com/drinami" target="_blank" rel="noopener noreferrer">Masahiko Inami</a></p>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn
+                color="primary"
+                class="btn--project"
+              >
+                <a href="https://signs0302.github.io/ukemochi/" target="_blank" rel="noopener noreferrer" class="btn--projecthp">Project Page</a>
+              </v-btn>
+              <v-btn
+                color="primary"
+                class="btn--project"
+              >
+                <a href="https://www.ipa.go.jp/jinzai/mitou/2019/gaiyou_in-2.html" target="_blank" rel="noopener noreferrer" class="btn--projecthp">News</a>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-layout>
+
+        </v-container>
+      </section>
+
+
+      <section class="top--wrapper">
+        <v-container>
+
+        <v-card
+          class="card">
           <h2>Publications</h2>
         </v-card>
 
-        <v-layout justify-center class="content--publications">
+        <v-layout justify-center class="content--publications" wrap>
 
-          <v-layout>
-          <v-card
-            class="content--work"
-          >
-            <v-card-title class="content--work--title">DeepTaste: Augmented Reality Gustatory Manipulation with GAN-based Real-time Food-to-food Translation</v-card-title>
+          <v-card class="content--work">
+            <v-card-title class="content--work--title">SSA-GAN: End-to-End Time-Lapse Video Generation with Spatial Self-Attention</v-card-title>
             <v-card-text>
-              <p>Kizashi Nakano*, <u><b>Daichi Horita*</b></u>, Nobuchika Sakata, Kiyoshi Kiyokawa, Keiji Yanai, and Takuji Narumi (*) equally contributed</p>
-              <p>Proc. IEEE International Symposium on Mixed and Augmented Reality (ISMAR) Beijing, China, Oct. 2019(Oral), acceptance rate x%.</p>
+              <p><u><b>Daichi Horita</b></u>, Keiji Yanai</p>
+              <p>Proc. of The 5th Asian Conference on Pattern Recognition (ACPR), Auckland, New Zealand, (2019/11), (poster). Acceptance rate 45%.</p>
             </v-card-text>
           </v-card>
-          </v-layout>
 
-          <br>
-          <v-card
-            class="content--work"
-          >
+          <v-card class="content--work">
             <v-card-title class="content--work--title">DeepTaste: Augmented Reality Gustatory Manipulation with GAN-based Real-time Food-to-food Translation</v-card-title>
             <v-card-text>
-              <p>Kizashi Nakano*, <u><b>Daichi Horita*</b></u>, Nobuchika Sakata, Kiyoshi Kiyokawa, Keiji Yanai, and Takuji Narumi (*) equally contributed</p>
-              <p>Proc. IEEE International Symposium on Mixed and Augmented Reality (ISMAR) Beijing, China, Oct. 2019(Oral), acceptance rate x%.</p>
+              <p>Kizashi Nakano*, <u><b>Daichi Horita*</b></u>, Nobuchika Sakata, Kiyoshi Kiyokawa, Keiji Yanai, and Takuji Narumi <u><b>(*) equally contributed</b></u></p>
+              <p>Proc. of IEEE International Symposium on Mixed and Augmented Reality (ISMAR) Beijing, China, (2019/10), (Oral). Acceptance rate TODO%.</p>
+            </v-card-text>
+          </v-card>
+
+
+          <v-card class="content--work">
+            <v-card-title class="content--work--title">Unseen Food Creation by Mixing Existing Food Images with Conditional StyleGAN</v-card-title>
+            <v-card-text>
+              <p><u><b>Daichi Horita</b></u>, Keiji Yanai</p>
+              <p>Proc. of ACMMM Workshop on Multimedia Assisted Dietary Management (MADIMA), Nice, France, (2019/10), (Oral).</p>
+            </v-card-text>
+          </v-card>
+
+          <v-card class="content--work">
+            <v-card-title class="content--work--title">Large-scale Twitter Food Photo Mining and Its Applications</v-card-title>
+            <v-card-text>
+              <p>Keiji Yanai, Kaimu Okamoto, Tetsuya Nagano and <u><b>Daichi Horita</b></u></p>
+              <p>Proc. of International Conference on Multimedia Big Data (BIGMM), Singapore, (2019/9), (invited paper).</p>
+            </v-card-text>
+          </v-card>
+
+          <v-card class="content--work">
+            <v-card-title class="content--work--title">Enchanting Your Noodles: GAN-based Real-time Food-to-Food Translation and Its Impact on Vision-induced Gustatory Manipulation</v-card-title>
+            <v-card-text>
+              <p>Kizashi Nakano*, <u><b>Daichi Horita*</b></u>, Nobuchika Sakata, Kiyoshi Kiyokawa, Keiji Yanai, and Takuji Narumi <u><b>(*) equally contributed</b></u></p>
+              <p>Proc. of IEEE Conference on Virtual Reality and 3D User Interfaces (IEEE VR), 1096-1097, Osaka, Japan, (2019/3) (poster).</p>
+            </v-card-text>
+          </v-card>
+
+          <v-card class="content--work">
+            <v-card-title class="content--work--title">Enchanting Your Noodles: A Gustatory Manipulation Interface by Using GAN-based Real-time Food-to-Food Translation</v-card-title>
+            <v-card-text>
+              <p>Kizashi Nakano*, <u><b>Daichi Horita*</b></u>, Nobuchika Sakata, Kiyoshi Kiyokawa, Keiji Yanai, and Takuji Narumi <u><b>(*) equally contributed</b></u></p>
+              <p>Proc. of IEEE Conference on Virtual Reality and 3D User Interfaces (IEEE VR), 1339-1340, Osaka, Japan, (2019/3) (demo).</p>
+            </v-card-text>
+          </v-card>
+
+
+          <v-card class="content--work">
+            <v-card-title class="content--work--title">Real-Time Image Classification and Transformation Apps on iOS by “Chainer2MPSNNGraph”</v-card-title>
+            <v-card-text>
+              <p>Yuki Izumi, <u><b>Daichi Horita</b></u>, Ryosuke Tanno and Keiji Yanai</p>
+              <p>Proc. of NIPS 2018 Workshop on Machine Learning on the Phone and other Consumer Devices (MLPCD), Motreal, Canada (2018/12).</p>
+            </v-card-text>
+          </v-card>
+
+          <v-card class="content--work">
+            <v-card-title class="content--work--title">CNN-based Photo Transformation for Improving Attractiveness of Ramen Photos</v-card-title>
+            <v-card-text>
+              <p><u><b>Daichi Horita</b></u>, Jaehyeong Cho, Takumi Ege, Keiji Yanai</p>
+              <p>Proc. of ACM Symposium on Virtual Reality Software and Technology (VRST), Tokyo, Japan, 115-116, (2018/11). (Poster paper).</p>
+            </v-card-text>
+          </v-card>
+
+
+          <v-card class="content--work">
+            <v-card-title class="content--work--title">Magical Rice Bowl: Real-time Food Category Changer</v-card-title>
+            <v-card-text>
+              <p>Ryosuke Tanno, <u><b>Daichi Horita</b></u>, Wataru Shimoda and Keiji Yanai</p>
+              <p>Proc. of ACM Multimedia, Seoul, Korea, 1244-1246, (2018/10). (Demo paper).</p>
+            </v-card-text>
+          </v-card>
+
+          <v-card class="content--work">
+            <v-card-title class="content--work--title">Food Category Transfer with Conditional Cycle GAN and a Large-scale Food Image Dataset</v-card-title>
+            <v-card-text>
+              <p><u><b>Daichi Horita</b></u>, Ryosuke Tanno, Wataru Shimoda and Keiji Yanai</p>
+              <p>Proc. of IJCAI Workshop on International Workshop on Multimedia Assisted Dietary Management (MADIMA), Stockholm, Sweden (2018/07).</p>
             </v-card-text>
           </v-card>
 
         </v-layout>
+        </v-container>
+      </section>
+
+      <section class="top--wrapper">
+        <v-container>
+
+        <v-card
+          class="card">
+          <h2>Medias</h2>
+        </v-card>
+
+        <v-layout justify-center class="content--publications" wrap>
+
+          <v-card class="content--work">
+            <v-card-title class="content--work--title">[テレビ出演] NEWS ZERO よみうりテレビ</v-card-title>
+            <v-card-text>
+              <p>[Nakano+ IEEEVR2019]の研究成果についてテレビ出演を行い解説を行いました.</p>
+              <p>```河西歩果が電気通信大の堀田大地さんを取材、そうめんをAR技術で焼きそばの映像に変えた。さらにカレーに変えると白いご飯をカレーに感じた。```</p>
+              <p>2019年5月24日（金）23時30分～0時30分</p>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn
+                color="primary"
+                class="btn--project"
+              >
+                <a href="https://tvtopic.goo.ne.jp/kansai/program/ytv/25518/822751/" target="_blank" rel="noopener noreferrer" class="btn--projecthp">Official Page</a>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+
+          <v-card class="content--work">
+            <v-card-title class="content--work--title">[ラジオ出演] J-WAVE「STEP ONE」「炒飯のAR」を見て白米を食べると…炒飯の味に!?</v-card-title>
+            <v-card-text>
+              <p>[Nakano+ IEEEVR2019]の研究成果についてラジオ出演を行い解説を行いました.</p>
+              <p>2019年4月3日（水）9時00分～13時00分</p>
+              <p>（関連ニュースサイト）</p>
+              <p><a href="https://shiropen.com/seamless/enchanting-your-noodles" target="_blank" rel="noopener noreferrer" class="btn--projecthp">奈良先端科学技術大学院大学など、そうめんをラーメンに錯覚させるARとGANを組み合わせたリアルタイム味覚操作システムを発表。白ご飯が焼飯にも, Seamless, (2019/3)</a></p>
+              <p><a href="https://www.moguravr.com/ar-ramen-yakisoba/" target="_blank" rel="noopener noreferrer" class="btn--projecthp">そうめんを食べるとラーメンや焼きそばの味 ARで錯覚を引き起こす, MoguraVR, (2019/3)</a></p>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn
+                color="primary"
+                class="btn--project"
+              >
+                <a href="https://www.j-wave.co.jp/blog/news/2019/04/43-ar.html" target="_blank" rel="noopener noreferrer" class="btn--projecthp">Official Page</a>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+
+
+          <v-card class="content--work">
+            <v-card-title class="content--work--title">[新聞掲載] 発掘！イグ・ノーベル賞（３７）電気通信大学　１杯のラーメンにかけるＡＩ</v-card-title>
+            <v-card-text>
+              <p>[Horita+ VRST2018]の研究成果についての記事が掲載されました.</p>
+              <p>2018年12月26日</p>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn
+                color="primary"
+                class="btn--project"
+              >
+                <a href="https://www.nikkan.co.jp/articles/view/00500813" target="_blank" rel="noopener noreferrer" class="btn--projecthp">Official Page</a>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+
+
+        </v-layout>
+
+        </v-container>
+      </section>
+
+      <section class="top--wrapper">
+        <v-container>
+
+        <v-card
+          class="card">
+          <h2>Projects</h2>
+        </v-card>
+
+        </v-container>
+      </section>
+
+      <section class="top--wrapper">
+        <v-container>
+
+        <v-card
+          class="card">
+          <h2>Awards</h2>
+        </v-card>
+
+        </v-container>
+      </section>
+
+      <section class="top--wrapper">
+        <v-container>
+
+        <v-card
+          class="card">
+          <h2>Hackathons</h2>
+        </v-card>
+
         </v-container>
       </section>
 
@@ -144,12 +346,15 @@ export default {
 
 .content--work{
   padding:20px 20px;
-  margin: 0 0 20px;
+  margin: 10px 0;
   width: 100%;
   box-shadow: 0 0 10px 0 rgba(255,255,255,.2);
 }
 
 .content--work--title{
   font-size: 80%;
+}
+.btn--projecthp{
+  color: black;
 }
 </style>
