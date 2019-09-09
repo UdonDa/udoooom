460515783ce5cb03745be341e76b802f3079153e
UdonDa <udoooon0727@gmail.com>
2019-09-10 01:35:27 +0900

    いいところ

--- pages/index.vue
+++ pages/index.vue
@@ -9,73 +9,105 @@
       sm8
       md6
     >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
+      <v-container>
+      <v-parallax
+        dark
+        src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+        width=100%
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <h1 class="display-2 font-weight-thin mb-4">Welcome to udoooom's site.</h1>
+        </v-row>
+      </v-parallax>
+      </v-container>
+
+
+
+      <section class="top--wrapper">
+        <v-container>
+      <v-card
+        class="card"
+      >
+        <h2>About</h2>
+
         <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
+
+          <v-layout justify-center>
+          <table>
+            <tr>
+              <th class="content--tag">Name:</th>
+              <th>Daichi Horita(堀田 大地)</th>
+            </tr>
+            <tr>
+              <th class="content--tag">Univ:</th>
+              <th>電気通信大学 柳井研究室 B4</th>
+            </tr>
+            <tr>
+              <th class="content--tag">GitHub:</th>
+              <th><a href="https://github.com/UdonDa" target="_blank" rel="noopener noreferrer">UdonDa</a></th>
+            </tr>
+            <tr>
+              <th class="content--tag">Twitter:</th>
+              <th><a href="https://twitter.com/udoooom" target="_blank" rel="noopener noreferrer">@udoooom</a></th>
+            </tr>
+            <tr>
+              <th class="content--tag">Blog:</th>
+              <th><a href="http://udoooom.hatenablog.com/" target="_blank" rel="noopener noreferrer">udoooom's diary</a></th>
+            </tr>
+            <tr>
+              <th class="content--tag">Email:</th>
+              <th>udooom0727[at]gmail.com</th>
+            </tr>
+          </table>
+          <!-- <hr class="my-3"> -->
+          </v-layout>
         </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
       </v-card>
-    </v-flex>
+      </v-container>
+      </section>
+
+      <section class="top--wrapper">
+        <v-container>
+
+        <v-card
+          class="card">
+          <h2>Publications</h2>
+        </v-card>
+
+        <v-layout justify-center class="content--publications">
+
+          <v-layout>
+          <v-card
+            class="content--work"
+          >
+            <v-card-title class="content--work--title">DeepTaste: Augmented Reality Gustatory Manipulation with GAN-based Real-time Food-to-food Translation</v-card-title>
+            <v-card-text>
+              <p>Kizashi Nakano*, <u><b>Daichi Horita*</b></u>, Nobuchika Sakata, Kiyoshi Kiyokawa, Keiji Yanai, and Takuji Narumi (*) equally contributed</p>
+              <p>Proc. IEEE International Symposium on Mixed and Augmented Reality (ISMAR) Beijing, China, Oct. 2019(Oral), acceptance rate x%.</p>
+            </v-card-text>
+          </v-card>
+          </v-layout>
+
+          <br>
+          <v-card
+            class="content--work"
+          >
+            <v-card-title class="content--work--title">DeepTaste: Augmented Reality Gustatory Manipulation with GAN-based Real-time Food-to-food Translation</v-card-title>
+            <v-card-text>
+              <p>Kizashi Nakano*, <u><b>Daichi Horita*</b></u>, Nobuchika Sakata, Kiyoshi Kiyokawa, Keiji Yanai, and Takuji Narumi (*) equally contributed</p>
+              <p>Proc. IEEE International Symposium on Mixed and Augmented Reality (ISMAR) Beijing, China, Oct. 2019(Oral), acceptance rate x%.</p>
+            </v-card-text>
+          </v-card>
+
+        </v-layout>
+        </v-container>
+      </section>
+
+  </v-flex>
+
   </v-layout>
 </template>
 
@@ -90,3 +122,34 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.top--wrapper{
+  margin:0;
+}
+.card{
+  /* width: 100%; */
+  padding:30px 90px;
+  text-align: center;
+}
+
+.content--tag{
+  padding-right:20px;
+  font-weight:400;
+}
+
+.content--publications{
+  /* background-color: */
+}
+
+.content--work{
+  padding:20px 20px;
+  margin: 0 0 20px;
+  width: 100%;
+  box-shadow: 0 0 10px 0 rgba(255,255,255,.2);
+}
+
+.content--work--title{
+  font-size: 80%;
+}
+</style>
